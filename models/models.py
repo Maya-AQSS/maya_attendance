34b755977c31118744b7c5f9b5af84e8f7c60ea5
Santi <santiselva2022@gmail.com>
2026-04-02 12:50:17 +0200

    feat: codigo comentado

--- models/models.py
+++ models/models.py
@@ -1,16 +1,16 @@
 from odoo import models, fields
 
 class MayaCoreEmployee(models.Model):
-    _inherit = 'maya_core.employee'
+    _inherit = 'maya_core.employee' #Hereda de employee de maya_core
 
-    id_tarjeta_rfid = fields.Integer(
+    id_tarjeta_rfid = fields.Integer( #Campo integer para el codigo RFID
         string='ID Tarjeta RFID',
         help="Identificador físico de la tarjeta para poder fichar",
         index=True,     
         required = True
     )
 
-    _sql_constraints = [
+    _sql_constraints = [ #Restricciones de la base de datos
         (
             'rfid_unique', 
             'unique(id_tarjeta_rfid)', 
