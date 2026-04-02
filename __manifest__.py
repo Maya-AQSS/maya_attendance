e90517d2ebc9f09bcdd707ab146bad19096f1e77
Santi <santiselva2022@gmail.com>
2026-04-02 19:18:46 +0200

    feat: Implemento nuevo modelo del modulo

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,15 @@
 {
-    'name': 'Maya-Attendance',
+    'name': 'Maya | Attendance',
     'version': '1.0',
-    'summary': 'Modulo Maya|Attendance',
+    'summary': 'Modulo Maya | Attendance para la gestion del fichaje',
     'depends': [
         'hr',
         'maya_core',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/views.xml',
     ],
     'installable': True,
-    'application': True,
     'license': 'LGPL-3',
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
-from . import models
+from . import attendance_employee
+from . import attendance
