4dd0e02c4344e4aefea86143ba1b6d6347f355a5
Santi <santiselva2022@gmail.com>
2026-04-28 12:15:48 +0200

    feat: implemento boton para bloquear el codigo rfid en hexadecimal y añado seccion para ver los ultimos fichajes

--- models/attendance_employee.py
+++ models/attendance_employee.py
@@ -8,22 +8,36 @@ class MayaCoreEmployee(models.Model):
         string='ID Tarjeta RFID',
         help="Identificador físico de la tarjeta para poder fichar",
         index=True,     
-        required = True,
     )
 
     id_rfid_decimal = fields.Char( #Campo que guarda el codigo rfid en formato decimal
         string="ID Tarjeta RFID Decimal",
         compute="_compute_rfid", #Calculamos el codigo a partir del rfid en hexadecimal
-        store = True
+        store = True,
     )
+
+    
+    esta_editando = fields.Boolean(default=False) # El "interruptor"
+
+    attendance_ids = fields.One2many(
+        'maya_attendance.attendance', 
+        'employee_id', 
+        string='Historial de Asistencias'
+    )
+
+    def action_toggle_edit(self): #Funcion para cambiar el estado de edicion
+        for record in self:
+            record.esta_editando = not record.esta_editando
 
     @api.depends("id_tarjeta_rfid") #Funcion para calcular el codigo rfid en decimal
     def _compute_rfid(self):
         for record in self:
             if record.id_tarjeta_rfid and isinstance(record.id_tarjeta_rfid, str):
                 # Si el codigo es string calculamos de hexadecimal a decimal el codigo
-                record.id_rfid_decimal = int(record.id_tarjeta_rfid, 16)
-               
+                try:
+                    record.id_rfid_decimal = int(record.id_tarjeta_rfid, 16)
+                except:
+                    record.id_rfid_decimal = "0"
             else:
                 record.id_rfid_decimal = 0
 
